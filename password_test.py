ad006981f6743df5bcb2085fd787369875540512
Langat05 <justuslangat78@gmail.com>
2020-01-24 22:01:27 +0300

    Created a test case to test the user_exist

--- password_test.py
+++ password_test.py
@@ -66,7 +66,20 @@ class TestUser(unittest.TestCase):
 
         found_user = User.find_by_name("Langat")
 
-        self.assertEqual(found_user.username,test_user.username)        
+        self.assertEqual(found_user.username,test_user.username)   
+
+    def test_user_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the user.
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Langat","justo01","justo12345") # new user
+        test_user.save_user()
+
+        user_exists = User.user_exist("Langat")
+
+        self.assertTrue(user_exists)         
 
 if __name__ ==  '__main__':
     unittest.main()
