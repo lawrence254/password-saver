a74c9a11078de4cb8472bfde1a7e5c96e61c1a10
Langat05 <justuslangat78@gmail.com>
2020-01-24 20:10:38 +0300

    Written the third test

--- password_test.py
+++ password_test.py
@@ -26,6 +26,17 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
+    
+    
+    def test_save_multiple_user(self):
+            '''
+            test_save_multiple_user to check if we can save multiple user
+            objects to our user_list
+            '''
+            self.new_user.save_user()
+            test_user = User("Langat","justo01","justo12345") # new user
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)    
 
 if __name__ ==  '__main__':
     unittest.main()
