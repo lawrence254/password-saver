dd57fb808b8c29642c860feea406d0f392943c23
Langat05 <justuslangat78@gmail.com>
2020-01-25 14:01:32 +0300

    created a function that delete a credential

--- run.py
+++ run.py
@@ -49,4 +49,10 @@ def save_credentials(credential):
     '''
     Function to save credential
     '''
-    credential.save_credential()           
+    credential.save_credential()   
+
+def del_credential(credential):
+    '''
+    Function to delete a credential
+    '''
+    credential.delete_credential()               
