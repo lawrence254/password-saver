5508f17aa37583bb5890f221af388dad7d85a67c
Langat05 <justuslangat78@gmail.com>
2020-01-25 13:44:15 +0300

    written a function that create new users

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3.7
+
+def create_user(name,username,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(name,username,password)
+    return new_user
